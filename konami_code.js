01e55c983106a7b0260a39ea4bcbbd1328982c6d
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-27 22:20:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let tracker = 0
 
 function init() {
-  let key = parseInt
   // Write your JavaScript code inside the init() function
+  function onKeyDown(event) {
+    console.log(event)
+  }
+  
+  document.addEventListener('keydow', onKeyDown)
 
 if (event.keyCode) === code[tracker]) {
   
