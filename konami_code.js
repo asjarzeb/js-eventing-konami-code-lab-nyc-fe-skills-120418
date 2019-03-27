c7bad524ff9c8a057d85304593e3c325da6b912b
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-27 22:45:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0
 
 function init() {
+  console.log('hi')
   // Write your JavaScript code inside the init() function
+  function onKeyDown(event) {
+    
+    if (event.keyCode === code[tracker]) {
+    tracker++
+    } else {
+      tracker = 0
+  }
+  
+  if (tracker === 10) {
+    alert('Nice Job')
+  }
+  
+}
+  document.addEventListener('keydown', onKeyDown)
 
 }
+
+init()
